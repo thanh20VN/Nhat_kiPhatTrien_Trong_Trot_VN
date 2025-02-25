68dad500195863ed9787c3773b32eadfb2faf8a2
thanh20VN <95450843+thanh20VN@users.noreply.github.com>
2025-02-25 09:50:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import csv
 import json
 from menudk import *
+import os
+import urllib.request
+import zipfile
+
+duong_dan = "data"
+
+if not os.path.exists(duong_dan):
+    url = "https://github.com/thanh20VN/Nhat_kiPhatTrien_Trong_Trot_VN/releases/download/data/data.zip"
+    ten_file = "data.zip"
+    
+    urllib.request.urlretrieve(url, ten_file)
+
+    with zipfile.ZipFile(ten_file, 'r') as zip_ref:
+        zip_ref.extractall(".")
+    os.remove('data.zip')
 
 with open("data/user.json", "r") as f:
     us = json.load(f)
